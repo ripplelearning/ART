90cbbb1fc411f784a392f3280cdd73bc3ab7ed28
ripplelearning <tristenbreitenfeldt@gmail.com>
2026-07-10 09:41:13 -0700

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,8 @@
+// dashboard.js
+import { switchView } from './navigation.js';
+import { appState } from './state.js';
+
+export function renderDashboard() {
+    // Logic for your "New Report" and "Open File" buttons
+    // Call switchView('builder') when "New Report" is clicked
+}
